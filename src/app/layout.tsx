8a79c3edbdf9c66babcac8379420795ee2778fe5
Fazal Samad <fazalsamad555@gmail.com>
2026-08-13 16:39:13 +0500

    Add Meta Pixel tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,29 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'AW-18358430212');`}
         </Script>
+        <Script id="meta-pixel">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '879826199948414');
+fbq('track', 'PageView');`}
+        </Script>
       </head>
       <body>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=879826199948414&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <QueryProvider>
           {children}
           <Toaster richColors position="top-right" />
