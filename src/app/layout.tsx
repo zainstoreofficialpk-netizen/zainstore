9f3f8685b1f5393e720fdf49fc879c7c64a25980
Fazal Samad <fazalsamad555@gmail.com>
2026-08-13 17:15:22 +0500

    Replace Meta Pixel with new pixel ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '879826199948414');
+fbq('init', '975051704544374');
 fbq('track', 'PageView');`}
         </Script>
       </head>
@@ -47,7 +47,7 @@ fbq('track', 'PageView');`}
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=879826199948414&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=975051704544374&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
